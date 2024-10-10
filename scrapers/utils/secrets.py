52a98ecac724b8c96fa7f0db7612f61f5fabc704
Daniel Gieseke <daniel.tyler.gieseke@gmail.com>
2024-10-10 12:52:31 -0400

    tweaking code to pass lint test

--- scrapers/utils/secrets.py
+++ scrapers/utils/secrets.py
@@ -2,7 +2,7 @@ import boto3
 import json
 
 
-def get_secret(secret_name, region='us-west-2'):
+def get_secret(secret_name, region="us-west-2"):
     """
     Retrieves secret value from a secret in AWS Secrets Manager.
 
@@ -14,7 +14,7 @@ def get_secret(secret_name, region='us-west-2'):
     - The value associated with the specified key.
     """
 
-    client = boto3.client(service_name='secretsmanager', region_name=region)
+    client = boto3.client(service_name="secretsmanager", region_name=region)
 
     try:
         secret_response = client.get_secret_value(SecretId=secret_name)
@@ -23,8 +23,8 @@ def get_secret(secret_name, region='us-west-2'):
         print(f"Error retrieving secret: {e}")
         return None
 
-    if 'SecretString' in secret_response:
-        secret = json.loads(secret_response['SecretString'])[secret_name]
+    if "SecretString" in secret_response:
+        secret = json.loads(secret_response["SecretString"])[secret_name]
         return secret
     else:
         print("Secret is binary or unavailable in string format.")
